03b86412ae941264135ccbb21473e37852a416ce
Ethan Ball <uthbees@gmail.com>
2024-08-24 23:02:56 -0600

    Basic desktop window
    The tutorial I'm using is located here: https://sotrh.github.io/learn-wgpu/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod window;
+
 fn main() {
-    println!("Hello, world!");
+    window::run();
 }
---
+++ src/window.rs
@@ -0,0 +1,41 @@
+use winit::{
+    event::{ElementState, Event, KeyEvent, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    // When wgpu hits any error, it panics with a generic message, while logging the real error via
+    // the log crate. Initializing the logger here allows us to see all the relevant information if
+    // something goes wrong.
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // TODO: Fix this properly once I learn more about Rust (match, at least).
+    #[allow(clippy::collapsible_match)]
+    event_loop
+        // TODO: Review this stuff once I learn more about Rust (callbacks, at least).
+        .run(move |event, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    event:
+                        KeyEvent {
+                            state: ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => control_flow.exit(),
+                _ => {}
+            },
+            _ => {}
+        })
+        .unwrap();
+}
