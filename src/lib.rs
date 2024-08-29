8149cefd1954c25fce0debc3703368092c117c95
Ethan Ball <uthbees@gmail.com>
2024-08-29 16:35:54 -0600

    Add web support

---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+// Use .expect() with a helpful message instead.
+#![warn(clippy::unwrap_used)]
+#![allow(clippy::missing_panics_doc)]
+
+use cfg_if::cfg_if;
+// TODO: Upgrade winit to the latest version.
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+use winit::{
+    event::{ElementState, Event, KeyEvent, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            // Set up logging for the web. We have to do this specially since env_logger doesn't support wasm.
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger.");
+        } else {
+            // Even if we're running normally, we still have to do special setup for logging. When
+            // wgpu hits any error, it panics with a generic message, while logging the real error
+            // via the log crate. We initialize env_logger here to make sure we always know what's
+            // going on.
+            env_logger::init();
+        }
+    }
+
+    let event_loop = EventLoop::new().expect("Failed to initialize main event loop.");
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .expect("Failed to create window.");
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use winit::dpi::LogicalSize;
+
+        // TODO: Review the next two "lines" once I learn more about Rust.
+        use winit::platform::web::WindowExtWebSys;
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| {
+                let destination = doc.get_element_by_id("canvas-holder")?;
+                let canvas = web_sys::Element::from(window.canvas()?);
+                destination.append_child(&canvas).ok()?;
+                Some(())
+            })
+            .expect("Couldn't append canvas to document body.");
+
+        // winit doesn't allow sizing with CSS, so we have to set the size manually when on web.
+        // Note that this sets the size of the canvas on web, not the window itself.
+        let _ = window.request_inner_size(LogicalSize::new(1000, 500));
+    }
+
+    // TODO: Use EventLoopExtWebSys::spawn() instead of run on web to avoid the JS exception trick.
+    // TODO: Review this stuff and fix this clippy error properly once I learn more about Rust (probably patterns and callbacks).
+    #[allow(clippy::collapsible_match)]
+    event_loop
+        .run(move |event, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    event:
+                        KeyEvent {
+                            state: ElementState::Pressed,
+                            physical_key: PhysicalKey::Code(KeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => control_flow.exit(),
+                _ => {}
+            },
+            _ => {}
+        })
+        .expect("Main event loop failed.");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-#![warn(clippy::all)]
-#![warn(clippy::pedantic)]
-// Use .expect() with a helpful message instead.
-#![warn(clippy::unwrap_used)]
-
-mod window;
-
 fn main() {
-    window::run();
+    simulation::run();
 }
