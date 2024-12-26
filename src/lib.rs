c900b262b0f21cfe02829b10fe35c2249463ccbe
Ethan Ball <uthbees@gmail.com>
2024-12-25 21:39:51 -0700

    Refactor: Extract handle_winit_event function

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use cfg_if::cfg_if;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 use winit::event::{Event, StartCause, WindowEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget};
 
 use crate::display::Display;
 pub use crate::ui::Ui;
@@ -15,6 +15,11 @@ pub mod display;
 pub mod ui;
 pub mod world;
 
+struct AppState<'a> {
+    display: Display<'a>,
+    next_frame_start_time: Instant,
+}
+
 /// Runs the application. Called by WASM directly.
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
@@ -23,58 +28,72 @@ pub async fn run() {
     let event_loop = EventLoop::new().expect("Failed to initialize main event loop");
     let window = display::create_window(&event_loop);
 
-    let mut display = Display::new(&window).await;
-
-    let mut next_frame_start_time = Instant::now();
-    let frame_length_as_d = Duration::from_secs_f32(FRAME_LENGTH);
+    let mut app_state = AppState {
+        display: Display::new(&window).await,
+        next_frame_start_time: Instant::now(),
+    };
 
     // TODO: Use EventLoopExtWebSys::spawn() instead of run() on web to avoid the JS exception trick.
     event_loop.set_control_flow(ControlFlow::Poll);
     event_loop
-        .run(move |event, control_flow| match event {
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                match display.render() {
-                    Ok(()) => {}
-                    // Reconfigure the surface if lost.
-                    Err(wgpu::SurfaceError::Lost) => display.configure_surface(),
-                    // If the system is out of memory, we should probably quit.
-                    Err(wgpu::SurfaceError::OutOfMemory) => control_flow.exit(),
-                    // The other errors (Outdated, Timeout) should be resolved by the next frame.
-                    Err(error) => eprintln!("{error:?}"),
-                }
-            }
-            Event::WindowEvent {
-                event: WindowEvent::Resized(physical_size),
-                ..
-            } => display.resize(physical_size),
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => control_flow.exit(),
-            Event::NewEvents(cause)
-                if cause == StartCause::Poll && Instant::now() >= next_frame_start_time =>
-            {
-                temp_update();
-
-                display.window().request_redraw();
-
-                // Increment the frame counter.
-                next_frame_start_time += frame_length_as_d;
-
-                // Drop any missed frames.
-                let mut proposed_next_frame_time = next_frame_start_time.add(frame_length_as_d);
-                while proposed_next_frame_time < Instant::now() {
-                    eprintln!("Dropped a frame.");
-                    next_frame_start_time = proposed_next_frame_time;
-                    proposed_next_frame_time = next_frame_start_time.add(frame_length_as_d);
-                }
-            }
-            _ => {}
-        })
+        .run(move |event, control_flow| handle_winit_event(&event, control_flow, &mut app_state))
         .expect("Main event loop failed");
+}
+
+fn handle_winit_event(
+    event: &Event<()>,
+    control_flow: &EventLoopWindowTarget<()>,
+    app_state: &mut AppState,
+) {
+    let frame_length_as_d = Duration::from_secs_f32(FRAME_LENGTH);
+    let AppState {
+        ref mut display,
+        mut next_frame_start_time,
+    } = app_state;
+
+    match *event {
+        Event::WindowEvent {
+            event: WindowEvent::RedrawRequested,
+            ..
+        } => {
+            match display.render() {
+                Ok(()) => {}
+                // Reconfigure the surface if lost.
+                Err(wgpu::SurfaceError::Lost) => display.configure_surface(),
+                // If the system is out of memory, we should probably quit.
+                Err(wgpu::SurfaceError::OutOfMemory) => control_flow.exit(),
+                // The other errors (Outdated, Timeout) should be resolved by the next frame.
+                Err(error) => eprintln!("{error:?}"),
+            }
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(physical_size),
+            ..
+        } => display.resize(physical_size),
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => control_flow.exit(),
+        Event::NewEvents(cause)
+            if cause == StartCause::Poll && Instant::now() >= next_frame_start_time =>
+        {
+            temp_update();
+
+            display.window().request_redraw();
+
+            // Increment the frame counter.
+            next_frame_start_time += frame_length_as_d;
+
+            // Drop any missed frames.
+            let mut proposed_next_frame_time = next_frame_start_time.add(frame_length_as_d);
+            while proposed_next_frame_time < Instant::now() {
+                eprintln!("Dropped a frame.");
+                next_frame_start_time = proposed_next_frame_time;
+                proposed_next_frame_time = next_frame_start_time.add(frame_length_as_d);
+            }
+        }
+        _ => {}
+    }
 }
 
 const FRAME_LENGTH: f32 = 1.0 / 60.0;
